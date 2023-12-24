9e8382e0d0b60231defa6f991f2becf566848644
Andrew Kotlov <andrewkotlov48@gmail.com>
2023-12-24 20:17:13 +0300

    add paralax scroll

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 1.5,
-    effects: true,
-    
-})
+if (ScrollTrigger.isTouch !==1) {
+
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true,
+    })
+
+    gsap.fromTo('.hero-section', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.hero-section',
+            start: 'center',
+            end: '820',
+            scrub: true
+        }
+    })
+
+    let itemsL = gsap.util.toArray('.gallary__left .gallary__item')
+
+    itemsL.forEach(item => {
+        gsap.fromTo(item, { x: -500, opacity: 0 }, {
+            opacity: 1, x: 0,
+            scrollTrigger: {
+                trigger: '.galary__item',
+                scrub: true
+            }
+        })
+    })
+
+}
